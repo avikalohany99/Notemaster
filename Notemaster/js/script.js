1cc2c74bdf574fe403334e22a4f7b08ad2c2405e
Avika Lohany <60224735+avikalohany99@users.noreply.github.com>
2021-05-26 22:56:00 -0400

    Added external javascript file

---
+++ Notemaster/js/script.js
@@ -0,0 +1,33 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        title: 'Notemaster',
+        note: {
+            title: '',
+            text: '',
+            num: '',
+        },
+        notes: [
+            {
+                title:'Visited Hawaii',
+                text: 'Loved the beaches',
+                date:  new Date(Date.now()).toLocaleString(),
+                num: '1'
+            }
+        ]
+    },
+    methods:{
+        addNote() {
+            let{ title, text, num } = this.note
+            this.notes.push({
+                title,
+                text,
+                date: new Date(Date.now()).toLocaleString(),
+                num,
+            })
+        },
+        removeNote(index) {
+            this.notes.splice(index, 1)
+        }
+    }
+})
